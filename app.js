689b2b3d007473f242d149d6fb9c71d388f0c6f4
abiydoni <abiydoni@gmail.com>
2026-06-21 18:13:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,2 @@
-module.exports = {
-  apps: [{
-    name: "wa-ab-gateway",
-    script: "./server.js",
-    watch: false,
-    ignore_watch: ["node_modules", "sessions", "tmp", "logs", "*.log"],
-    env: {
-      NODE_ENV: "production",
-      PORT: 3456,
-      TZ: "Asia/Jakarta" // Menyesuaikan dengan zona waktu di server.js
-    },
-    // Restart otomatis jika memori melebihi 500MB untuk menghindari crash
-    max_memory_restart: "500M",
-    error_file: "./logs/err.log",
-    out_file: "./logs/out.log",
-    merge_logs: true,
-    time: true
-  }]
-};
+// File app.js ini dipanggil untuk menjalankan server
+require('./server.js');
